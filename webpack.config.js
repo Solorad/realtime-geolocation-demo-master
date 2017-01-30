fb663d6362a34bdb3212f8a88f6093e2dc6fe303
emorenkov <emorenkov@luxoft.com>
2017-01-30 19:21:17 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,138 @@
-const path = require('path');
 const webpack = require('webpack');
+const path = require('path');
 
+const sourcePath = path.join(__dirname, './client');
+const staticsPath = path.join(__dirname, './static');
 
-var node_dir = __dirname + '/node_modules';
-const nodeEnv = process.env.NODE_ENV || 'development';
-const isProd = nodeEnv === 'production';
+module.exports = function (env) {
+  const nodeEnv = env && env.prod ? 'production' : 'development';
+  const isProd = nodeEnv === 'production';
 
-const plugins = [
-  new webpack.NamedModulesPlugin(),
-  new webpack.optimize.CommonsChunkPlugin({ name: "c", filename: "c.js" }),
-  new webpack.HotModuleReplacementPlugin()
-];
-
-if (isProd) {
-  plugins.push(
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
+  const plugins = [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+      filename: 'vendor.bundle.js'
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
-      },
-      output: {
-        comments: false
-      },
-    })
-  );
-} else {
-  plugins.push(
-    new webpack.HotModuleReplacementPlugin()
-  );
-}
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    }),
+    new webpack.NamedModulesPlugin(),
+  ];
 
-module.exports = {
-  entry: './src/main/resources/static/js/index.js',
-  devtool: isProd ? 'source-map' : 'eval',
-  resolve: {
-    alias: {
-      'stompjs': node_dir + '/stompjs/lib/stomp.js',
-    }
-  },
-  output: {
-    path: __dirname,
-    filename: './src/main/resources/static/built/bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: path.join(__dirname, '.'),
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015', 'react']
+  if (isProd) {
+    plugins.push(
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true,
+        },
+        output: {
+          comments: false,
+        },
+      })
+    );
+  } else {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin()
+    );
+  }
+
+  return {
+    devtool: isProd ? 'source-map' : 'eval',
+    context: sourcePath,
+    entry: {
+      js: './server.js',
+      vendor: ['react']
+    },
+    output: {
+      path: staticsPath,
+      filename: '[name].bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'file-loader',
+            query: {
+              name: '[name].[ext]'
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader'
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+      modules: [
+        path.resolve(__dirname, 'node_modules'),
+        sourcePath
+      ]
+    },
+
+    plugins,
+
+    performance: isProd && {
+      maxAssetSize: 100,
+      maxEntrypointSize: 300,
+      hints: 'warning',
+    },
+
+    stats: {
+      colors: {
+        green: '\u001b[32m',
+      }
+    },
+
+    devServer: {
+      contentBase: './client',
+      historyApiFallback: true,
+      port: 3000,
+      compress: isProd,
+      inline: !isProd,
+      hot: !isProd,
+      stats: {
+        assets: true,
+        children: false,
+        chunks: false,
+        hash: false,
+        modules: false,
+        publicPath: false,
+        timings: true,
+        version: false,
+        warnings: true,
+        colors: {
+          green: '\u001b[32m',
         }
       },
-      {
-        test: /\.png$/,
-        loader: 'file'
-      },
-      {
-        test: /\.less$/,
-        use: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'less-loader'
-        ]
-      }
-
-    ]
-  }
+    }
+  };
 };
